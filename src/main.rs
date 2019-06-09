5460e7c18179bcb729f5e48348a210ee9d5f55df
Morelatto <morelatto@pm.me>
2019-06-09 14:16:58 -0300

    Added config file argument.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ const USAGE: &'static str = "
 LastFM rust scrobbler. Scrobbles all mp3 in a folder.
 
 Usage:
-  rust-scrobbler <folder>... [--loop]
+  rust-scrobbler <folder>... [--loop] [--config=FILE]
   rust-scrobbler (-h | --help)
   rust-scrobbler --version
 
@@ -22,15 +22,17 @@ Arguments:
   folders       Folders without trailing slash with mp3 files to scrobble.
 
 Options:
-  --loop        Keep looping on folders after scrobbling everything once.
-  -h --help     Show this message.
-  --version     Show version.
+  --loop            Keep looping on folders after scrobbling everything once.
+  --config=FILE     Configuration file path [default: app.conf].
+  -h --help         Show this message.
+  --version         Show version.
 ";
 
 #[derive(Debug, Deserialize)]
 struct Args {
     arg_folder: Vec<String>,
     flag_loop: bool,
+    flag_config: Option<String>,
 }
 
 fn main() {
@@ -38,8 +40,7 @@ fn main() {
         .and_then(|d| d.version(Some("0.2.0".to_string())).deserialize())
         .unwrap_or_else(|e| e.exit());
 
-    // TODO pass config file path on arguments
-    let configuration = reader::from_file(Path::new("app.conf")).expect("Failed to load app.conf");
+    let configuration = reader::from_file(Path::new(&args.flag_config.unwrap())).unwrap();
     let scrobbler = scrobbler::ScrobbleClient::new(&configuration);
 
     loop {
@@ -56,6 +57,7 @@ fn main() {
                         let title = tag.title().unwrap();
 
                         scrobbler.now_playing(artist, title, album);
+                        // TODO get real duration of track and add cli option to use it instead
                         wait(tag.duration().unwrap_or_else(|| 30));
                         scrobbler.scrobble(artist, title, album);
                     }
